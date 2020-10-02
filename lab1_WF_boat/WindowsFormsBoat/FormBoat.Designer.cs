4d637e7c7971e553da2214f0b7d9abe622fdfab5
Mariya Pavlova <mariyapavlova2001@mail.ru>
2020-10-03 02:36:14 +0400

    Lab 1 is ready

---
+++ lab1_WF_boat/WindowsFormsBoat/FormBoat.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsBoat
+{
+	partial class FormBoat
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBoat));
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.pictureBoxBoat = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBoat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(904, 12);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(215, 91);
+            this.buttonCreate.TabIndex = 5;
+            this.buttonCreate.Text = "Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+            this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonLeft.Location = new System.Drawing.Point(904, 495);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(40, 40);
+            this.buttonLeft.TabIndex = 6;
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonDown.Location = new System.Drawing.Point(984, 496);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(40, 40);
+            this.buttonDown.TabIndex = 7;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+            this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonRight.Location = new System.Drawing.Point(1061, 496);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(40, 40);
+            this.buttonRight.TabIndex = 8;
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonUp.Location = new System.Drawing.Point(984, 436);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(40, 40);
+            this.buttonUp.TabIndex = 9;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonMove_Click);
+            // 
+            // pictureBoxBoat
+            // 
+            this.pictureBoxBoat.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxBoat.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxBoat.Name = "pictureBoxBoat";
+            this.pictureBoxBoat.Size = new System.Drawing.Size(876, 532);
+            this.pictureBoxBoat.TabIndex = 4;
+            this.pictureBoxBoat.TabStop = false;
+            // 
+            // FormBoat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 556);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.pictureBoxBoat);
+            this.Name = "FormBoat";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBoat)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Button buttonCreate;
+		private System.Windows.Forms.Button buttonLeft;
+		private System.Windows.Forms.Button buttonDown;
+		private System.Windows.Forms.Button buttonRight;
+		private System.Windows.Forms.Button buttonUp;
+		private System.Windows.Forms.PictureBox pictureBoxBoat;
+	}
+}
+
---
+++ lab1_WF_boat/WindowsFormsBoat/FormBoat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsBoat
+{
+	public partial class FormBoat : Form
+	{
+
+		private Catamaran boat;
+
+		public FormBoat()
+		{
+			InitializeComponent();
+		}
+
+		private void Draw()
+		{
+			Bitmap bmp = new Bitmap(pictureBoxBoat.Width, pictureBoxBoat.Height);
+			Graphics gr = Graphics.FromImage(bmp);
+			boat.DrawTransport(gr);
+			pictureBoxBoat.Image = bmp;
+
+		}
+
+		private void buttonCreate_Click(object sender, EventArgs e)
+		{
+			Random rnd = new Random();
+			boat = new Catamaran(rnd.Next(100, 300), rnd.Next(1000, 2000), Color.Aqua, Color.Gray, true, true);
+			boat.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxBoat.Width,
+pictureBoxBoat.Height);
+			Draw();
+		}
+
+		private void buttonMove_Click(object sender, EventArgs e)
+		{
+			//получаем имя кнопки
+			string name = (sender as Button).Name;
+			switch (name)
+			{
+				case "buttonUp":
+					boat.MoveTransport(Direction.Up);
+					break;
+				case "buttonDown":
+					boat.MoveTransport(Direction.Down);
+					break;
+				case "buttonLeft":
+					boat.MoveTransport(Direction.Left);
+					break;
+				case "buttonRight":
+					boat.MoveTransport(Direction.Right);
+					break;
+			}
+			Draw();
+		}
+
+		
+	}
+}
